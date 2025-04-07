b1ad7eb632c4762925a48e4e207c80b9323d381a
RaunakKunti <rounakkunti@gmail.com>
2025-04-07 12:19:36 +0530

    currency converter

---
+++ sixth_currency_cconvert/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+// https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api/v1/currencies/usd.json
+
+import { useState,useEffect } from "react";
+
+function useCurrencyInfo(currency){
+    const [data,setData] = useState({}) // we are passig the empty object because if any value is not provided then we don't want our app crashed when apply loop
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api/v1/currencies/${currency}.json`)
+        .then((res)=> res.json())
+        .then((res)=> setData(res[currency]))
+        console.log(data);
+    },[currency])
+    return data
+}
+
+export default useCurrencyInfo;
