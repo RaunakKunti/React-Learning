0fcfdb59b1152914027385e679f02ec4f71cdd27
Raunak <rounakkunti@gmail.com>
2025-06-10 12:16:23 +0530

    add update

--- tenthTodo/src/App.jsx
+++ tenthTodo/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
-  }
+  } 
 
   const updateTodo = (id, todo) => {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id ===id ? todo : prevTodo)))
